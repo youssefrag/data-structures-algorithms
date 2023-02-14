37fc3dc231481aeabd10ecf2658b133d862254d4
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-14 15:09:38 -0500

    finish exc 1

--- node.js
+++ node.js
@@ -1,28 +1,41 @@
-const { performance } = require("perf_hooks");
+// const { performance } = require("perf_hooks");
 
-const nemo = ["nemo"];
+// const nemo = ["nemo"];
 
-const everyone = [
-  "dory",
-  "bruce",
-  "marlin",
-  "gill",
-  "bloat",
-  "nigel",
-  "squirt",
-  "darla",
-  "hank",
-  "nemo",
-];
+// const everyone = [
+//   "dory",
+//   "bruce",
+//   "marlin",
+//   "gill",
+//   "bloat",
+//   "nigel",
+//   "squirt",
+//   "darla",
+//   "hank",
+//   "nemo",
+// ];
 
-const large = new Array(100).fill("nemo");
+// const large = new Array(100).fill("nemo");
 
-const findNemo = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === "nemo") {
-      console.log("WE FOUND NEMO");
-    }
-  }
+// const findNemo = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === "nemo") {
+//       console.log("WE FOUND NEMO");
+//     }
+//   }
+// };
+
+// findNemo(large); // O(n) --> Linear Time
+
+// const compressFirstBox = (boxes) => {
+//   console.log(boxes[0]);
+// }; // O(1) --> Constant Time
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+const logFirstTwoBoxes = (boxes) => {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
 };
 
-findNemo(large); // O(n) --> Linear Time
+logFirstTwoBoxes(boxes); // O(2)
