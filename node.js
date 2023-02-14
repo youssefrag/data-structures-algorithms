9d433fb0fe9a530c5bbe4a7854dd34c0b5c7f889
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-14 15:59:25 -0500

    big o rule 2

--- node.js
+++ node.js
@@ -2,40 +2,42 @@
 
 // const nemo = ["nemo"];
 
-// const everyone = [
-//   "dory",
-//   "bruce",
-//   "marlin",
-//   "gill",
-//   "bloat",
-//   "nigel",
-//   "squirt",
-//   "darla",
-//   "hank",
-//   "nemo",
-// ];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
 
 // const large = new Array(100).fill("nemo");
 
-// const findNemo = (array) => {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === "nemo") {
-//       console.log("WE FOUND NEMO");
-//     }
-//   }
-// };
+const findNemo = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log("running");
+    if (array[i] === "nemo") {
+      console.log("WE FOUND NEMO");
+      break;
+    }
+  }
+};
 
-// findNemo(large); // O(n) --> Linear Time
+findNemo(everyone); // O(n) --> Linear Time
 
 // const compressFirstBox = (boxes) => {
 //   console.log(boxes[0]);
 // }; // O(1) --> Constant Time
 
-const boxes = [0, 1, 2, 3, 4, 5];
+// const boxes = [0, 1, 2, 3, 4, 5];
 
-const logFirstTwoBoxes = (boxes) => {
-  console.log(boxes[0]); // O(1)
-  console.log(boxes[1]); // O(1)
-};
+// const logFirstTwoBoxes = (boxes) => {
+//   console.log(boxes[0]); // O(1)
+//   console.log(boxes[1]); // O(1)
+// };
 
-logFirstTwoBoxes(boxes); // O(2)
+// logFirstTwoBoxes(boxes); // O(2)
