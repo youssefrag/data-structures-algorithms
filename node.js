14058fb89a7c5fa8b2cd6201ce5fb2c0082a671a
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-14 14:49:05 -0500

    define linear rime

--- node.js
+++ node.js
@@ -18,14 +18,11 @@ const everyone = [
 const large = new Array(100).fill("nemo");
 
 const findNemo = (array) => {
-  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("WE FOUND NEMO");
     }
   }
-  let t1 = performance.now();
-  console.log("Call to find Nemo took", t1 - t0, "milliseconds");
 };
 
-findNemo(large);
+findNemo(large); // O(n) --> Linear Time
